edd9f220cfd640bdadb05bb0e9eff8d5bb86eb53
DineshShiva <101635905+DineshShiva@users.noreply.github.com>
2022-03-15 16:06:53 +0530

    Create Implementing an array.py

---
+++ Implementing an array.py
@@ -0,0 +1,51 @@
+class funArray:
+    def __init__(self):
+        self.leng =0 
+        self.dis = {}
+        
+    def get(self,index):
+        return self.dis[index]
+    
+    def push(self,value):
+        self.dis[self.leng]=value
+        self.leng+=1
+        
+    def pop(self):
+        del self.dis[self.leng-1]
+        self.leng-=1
+        
+    def delete(self,index):
+        for i in range(index,self.leng-1):
+            self.dis[i] = self.dis[i+1]
+        del self.dis[self.leng-1]
+        self.leng-=1
+        
+    def __str__(self):
+        return str(self.dis)
+    
+    
+
+array = funArray()
+
+array.push('d')
+
+array.push('i')
+
+array.push('n')
+
+array.push('e')
+
+array.push('s')
+
+print(array)
+
+print(array.get(2))
+
+array.pop()
+
+print(array)
+
+array.delete(1)
+
+for i,j in array.dis.items():
+    print(i,j)
